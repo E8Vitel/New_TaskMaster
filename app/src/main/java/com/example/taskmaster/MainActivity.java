16a4867164cd3244921df3330ffbc9444bc21457
E8Vitel <viktoralvz@gmail.com>
2023-11-11 21:21:27 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.taskmaster.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new TareaFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.inicio) {
+                replaceFragment(new TareaFragment());
+            } else if (itemId == R.id.config) {
+                replaceFragment(new ConfigFragment());
+            } else if (itemId == R.id.about) {
+                replaceFragment(new AboutFragment());
+            } else if (itemId == R.id.cuenta) {
+                replaceFragment(new CuentaFragment());
+            }
+
+            return true;
+
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
